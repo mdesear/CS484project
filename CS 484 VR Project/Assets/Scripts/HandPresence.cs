bdfee6d40d04a36164d9d0cfe2e6b4154941916c
oliverfliegler <64721927+oliverfliegler@users.noreply.github.com>
2022-05-01 15:49:29 -0700

    teleport and signs

--- CS 484 VR Project/Assets/Scripts/HandPresence.cs
+++ CS 484 VR Project/Assets/Scripts/HandPresence.cs
@@ -1,64 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.XR;
+using UnityEngine.XR.Interaction.Toolkit;
+
+[RequireComponent(typeof(ActionBasedController))]
 
 public class HandPresence : MonoBehaviour
 {
-	public GameObject handModelPrefab;
-	public InputDeviceCharacteristics controllerCharacteristics;
+	ActionBasedController controller;
 	
-	private InputDevice targetDevice;
-	private GameObject spawnedHandModel;
-	private Animator handAnimator;
+	public Hand hand;
 	
     // Start is called before the first frame update
     void Start()
-    {	
-		TryInitialize();
+    {
+			controller = GetComponent<ActionBasedController>();
     }
-	
-	void TryInitialize()
-	{
-		List<InputDevice> devices = new List<InputDevice>();
-		
-		InputDevices.GetDevicesWithCharacteristics(controllerCharacteristics, devices);
-		
-		spawnedHandModel = Instantiate(handModelPrefab, transform);
-		handAnimator = spawnedHandModel.GetComponent<Animator>();
-	}
-	
-	void UpdateHandAnimation() 
-	{
-		if(targetDevice.TryGetFeatureValue(CommonUsages.trigger, out float triggerValue))
-		{
-			handAnimator.SetFloat("Trigger", triggerValue);
-		} 
-		else 
-		{
-			handAnimator.SetFloat("Trigger", 0);
-		}
-		
-		if(targetDevice.TryGetFeatureValue(CommonUsages.grip, out float gripValue))
-		{
-			handAnimator.SetFloat("Grip", gripValue);
-		} 
-		else 
-		{
-			handAnimator.SetFloat("Grip", 0);
-		}
-	}
-	
+
     // Update is called once per frame
     void Update()
     {
-		if(!targetDevice.isValid)
-		{
-			TryInitialize();
-		} 
-		else 
-		{
-			UpdateHandAnimation();
-		}
+		hand.SetGrip(controller.selectAction.action.ReadValue<float>());
+		hand.SetTrigger(controller.activateAction.action.ReadValue<float>()); 
     }
 }
---
+++ CS 484 VR Project/Assets/Scripts/PhysicsHands.cs
@@ -0,0 +1,105 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PhysicsHands : MonoBehaviour
+{ 
+	[Header("PID")]
+	[SerializeField] float frequency = 50f;
+	[SerializeField] float damping = 1f;
+	[SerializeField] float rotFrequency = 100f;
+	[SerializeField] float rotDamping = 0.9f;
+	[SerializeField] Rigidbody playerRigidbody;
+	[SerializeField] Transform target;
+	
+	[Space]
+	[Header("Springs")]
+	[SerializeField] float climbForce = 1000f;
+	[SerializeField] float climbDrag = 500f;
+	
+	Vector3 _previousPosition;
+	Rigidbody _rigidbody;
+	bool _isColliding;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+		transform.position = target.position;
+		transform.rotation = target.rotation;
+		
+        _rigidbody = GetComponent<Rigidbody>();
+		_rigidbody.maxAngularVelocity = float.PositiveInfinity;
+		_previousPosition = transform.position;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        PIDMovement();
+		PIDRotation();
+		if (_isColliding) HookesLaw();
+    }
+	
+	void PIDMovement()
+	{
+		float kp = (6f * frequency) * (6f * frequency) * 0.25f;
+		float kd = 4.5f * frequency * damping;
+		float g = 1 / (1 + kd * Time.fixedDeltaTime + kp * Time.fixedDeltaTime * Time.fixedDeltaTime);
+		float ksg = kp * g;
+		float kdg = (kd + kp * Time.fixedDeltaTime) * g;
+		Vector3 force = (target.position - transform.position) * ksg + (playerRigidbody.velocity - _rigidbody.velocity) * kdg;
+		_rigidbody.AddForce(force, ForceMode.Acceleration);
+	}
+	
+	void PIDRotation()
+	{
+		float kp = (6f * rotFrequency) * (6f * rotFrequency) * 0.25f;
+		float kd = 4.5f * rotFrequency * rotDamping;
+		float g = 1 / (1 + kd * Time.fixedDeltaTime + kp * Time.fixedDeltaTime * Time.fixedDeltaTime);
+		float ksg = kp * g;
+		float kdg = (kd + kp * Time.fixedDeltaTime) * g;
+		Quaternion q = target.rotation * Quaternion.Inverse(transform.rotation);
+		if (q.w < 0) 
+		{
+			q.x = -q.x;
+			q.y = -q.y;
+			q.z = -q.z;
+			q.w = -q.w;
+		}
+		q.ToAngleAxis(out float angle, out Vector3 axis);
+		axis.Normalize();
+		axis *= Mathf.Deg2Rad;
+		Vector3 torque = ksg * axis * angle + -_rigidbody.angularVelocity * kdg;
+		_rigidbody.AddTorque(torque, ForceMode.Acceleration);
+	}
+	
+	void HookesLaw() 
+	{
+		Vector3 displacementFromResting = transform.position - target.position;
+		Vector3 force = displacementFromResting * climbForce;
+		float drag = GetDrag();
+		
+		playerRigidbody.AddForce(force, ForceMode.Acceleration);
+		playerRigidbody.AddForce(drag * -playerRigidbody.velocity * climbDrag, ForceMode.Acceleration);
+	}
+	
+	float GetDrag()
+	{
+		Vector3 handVelocity = (target.localPosition - _previousPosition) / Time.fixedDeltaTime;
+		float drag = 1 / handVelocity.magnitude + 0.01f;
+		drag = drag > 1 ? 1 : drag;
+		drag = drag < 0.03f ? 0.03f : drag;
+		_previousPosition = transform.position;
+		return drag; 
+	}
+	
+	void OnCollisionEnter(Collision collider)
+	{
+		_isColliding = true;
+	}
+	
+	void OnCollisionExit(Collision collider)
+	{
+		_isColliding = false;
+	}
+}
